9662f47566ebee47e52b962b1e00d912088574d2
Nephi Calvin Bandela <nimble33@zimbra.ccs.neu.edu>
2016-07-18 19:53:45 -0400

    A* todo

--- HW2/src/main/java/Main.java
+++ HW2/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import pacman.Executor;
 import entrants.pacman.nephi.MyPacMan_BFS;
+import entrants.pacman.nephi.MyPacMan;
 import entrants.pacman.nephi.MyPacMan_AStar;
 import entrants.pacman.nephi.MyPacMan_DFS;
 import examples.commGhosts.POCommGhosts;
@@ -10,19 +11,28 @@ import pacman.controllers.examples.RandomGhosts;
 public class Main {
 
 	public static void main(String[] args) {
-
-		Executor executor = new Executor(true, true);
+		
+		
+		
 		//Default
-		//executor.runGameTimed(new POPacMan(), new POCommGhosts(50), true);
+		//Partially Observable
+		//Executor executor = new Executor(true, true);
+		//executor.runGameTimed(new MyPacMan(), new POCommGhosts(50), true);
 		
 		//1.DFS Implementation - Uninformed Search
-		executor.runGameTimed(new MyPacMan_DFS(), new RandomGhosts(), true);
-
+		//Partially Observable
+		//Executor executor = new Executor(true, true);
+		//executor.runGameTimed(new MyPacMan_DFS(), new RandomGhosts(), true);
+		
 		//2.BFS Implementation - Uninformed Search
+		//Partially Observable
+		//Executor executor = new Executor(true, true);
 		// executor.runGameTimed(new MyPacMan_BFS(), new POCommGhosts(50), true);
 		
 		//3.AStar Implementation - Informed Search
-		//executor.runGameTimed(new MyPacMan_AStar(), new StarterGhosts(), true);
+		//Fully Observable
+		Executor executor = new Executor(false, true);
+		executor.runGameTimed(new MyPacMan_AStar(), new POCommGhosts(50), true);
 		
 	}
 }
