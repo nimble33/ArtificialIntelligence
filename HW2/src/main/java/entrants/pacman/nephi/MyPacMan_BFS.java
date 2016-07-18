d86607b029626aee1b94a3f8daf2da83b48894b0
Nephi Calvin Bandela <nimble33@zimbra.ccs.neu.edu>
2016-07-18 01:38:07 -0400

    BFS - Draft version

--- HW2/src/main/java/entrants/pacman/nephi/MyPacMan_BFS.java
+++ HW2/src/main/java/entrants/pacman/nephi/MyPacMan_BFS.java
@@ -1,20 +1,71 @@
 package entrants.pacman.nephi;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Map.Entry;
 
 import pacman.controllers.PacmanController;
 import pacman.game.Constants.MOVE;
+import pacman.game.internal.Node;
 import pacman.game.Game;
 
 /*
- * This is the class you need to modify for your entry. In particular, you need to
- * fill in the getMove() method. Any additional classes you write should either
- * be placed in this package or sub-packages (e.g., entrants.pacman.username).
+ * BFS Implementation
  */
+
 public class MyPacMan_BFS extends PacmanController {
-    private MOVE myMove = MOVE.NEUTRAL;
+ 
+    
+	private boolean isPlaying= false; //game state
+	private MOVE myMove;
+	private boolean visited[]; //visited nodes?
+	private Queue<Node> bfsQueue=new LinkedList<Node>(); //Queue to maintain nodes
 
     public MOVE getMove(Game game, long timeDue) {
+    	int size = 0;
+    	Node[] bfsGraph;
         //Place your game logic here to play the game as Ms Pac-Man
-
-        return myMove;
+    	if(isPlaying!=true ||  game.wasPacManEaten()){
+			int current = game.getPacmanCurrentNodeIndex(); //Current Pacman index
+			 bfsGraph= game.getCurrentMaze().graph; // current maze graph - set of nodes
+			Node currentPacManNode= bfsGraph[current]; //current node in the graph
+			bfsQueue.add(currentPacManNode); // root node
+			size=bfsGraph.length;
+			visited=new boolean[size];//initialize to maze length
+			visited[currentPacManNode.nodeIndex]=true; 
+			isPlaying=true;   		
+		}
+    
+    		Node n=bfsQueue.remove();    		
+    		Entry<MOVE, Integer> selectedNode;    		
+    		while((selectedNode = selectFromNeighbors(n))!= null)	{
+    			visited[selectedNode.getValue()]=true;
+    			Node entry=game.getCurrentMaze().graph[selectedNode.getValue()];
+    			bfsQueue.add(entry);
+    			//System.out.println(selectedNode.getKey());
+    			myMove = selectedNode.getKey();    					
+    		}
+    		clearNodes(size,game);
+    	return myMove;
     }
+
+	private void clearNodes(int size, Game game) {
+		int i=0;
+		while(i<size)
+		{
+			Node n= game.getCurrentMaze().graph[i];
+			visited[n.nodeIndex]=false;
+			i++;
+		}
+	}
+
+	private Entry<MOVE, Integer> selectFromNeighbors(Node n) {
+		// TODO Auto-generated method stub
+		for (Entry<MOVE, Integer> neighbour: n.neighbourhood.entrySet()) {
+			if (!visited[neighbour.getValue()]) {
+				return neighbour;
+			}
+		}
+		return null;
+	}
 }
+
