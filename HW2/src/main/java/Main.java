6650e8172ac36caa569fc92d3fa2fbcfe5446d67
Nephi Calvin Bandela <nimble33@zimbra.ccs.neu.edu>
2016-07-17 22:01:31 -0400

    DFS working

--- HW2/src/main/java/Main.java
+++ HW2/src/main/java/Main.java
@@ -1,28 +1,28 @@
 import pacman.Executor;
-//import examples.poPacMan.POPacMan;
-import entrants.pacman.nephi.MyPacMan_DFS;
 import entrants.pacman.nephi.MyPacMan_BFS;
 import entrants.pacman.nephi.MyPacMan_AStar;
 import entrants.pacman.nephi.MyPacMan_DFS;
 import examples.commGhosts.POCommGhosts;
-
-
+import pacman.controllers.examples.RandomGhosts;
 /**
  * Created by pwillic on 06/05/2016.
  */
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Executor executor = new Executor(true, true);
-        //Default
-        //executor.runGameTimed(new POPacMan(), new POCommGhosts(50), true);
-        //1.DFS Implementation - Uninformed Search
-        executor.runGameTimed(new MyPacMan_DFS(), new POCommGhosts(50), true);
-        //2.BFS Implementation - Uninformed Search
-        //executor.runGameTimed(new MyPacMan_BFS(), new POCommGhosts(50), true);
-        //3.AStar Implementation - Informed Search
-        //executor.runGameTimed(new MyPacMan_AStar(), new POCommGhosts(50), true);
-        
-    }
+		Executor executor = new Executor(true, true);
+		//Default
+		//executor.runGameTimed(new POPacMan(), new POCommGhosts(50), true);
+		
+		//1.DFS Implementation - Uninformed Search
+		executor.runGameTimed(new MyPacMan_DFS(), new RandomGhosts(), true);
+
+		//2.BFS Implementation - Uninformed Search
+		// executor.runGameTimed(new MyPacMan_BFS(), new POCommGhosts(50), true);
+		
+		//3.AStar Implementation - Informed Search
+		//executor.runGameTimed(new MyPacMan_AStar(), new StarterGhosts(), true);
+		
+	}
 }
--- HW2/src/main/java/entrants/pacman/nephi/MyPacMan_DFS.java
+++ HW2/src/main/java/entrants/pacman/nephi/MyPacMan_DFS.java
@@ -1,21 +1,69 @@
 package entrants.pacman.nephi;
 
 import pacman.controllers.PacmanController;
-import pacman.game.Constants.MOVE;
+import pacman.game.Constants.*;
 import pacman.game.Game;
+import pacman.game.internal.Node;
+import pacman.game.Constants;
+import java.util.Map.Entry;
+import java.util.Stack;
 
 /*
- * This is the class you need to modify for your entry. In particular, you need to
- * fill in the getMove() method. Any additional classes you write should either
- * be placed in this package or sub-packages (e.g., entrants.pacman.username).
+ * PacMan Search based on DFS
  */
 public class MyPacMan_DFS extends PacmanController {
-    private MOVE myMove = MOVE.NEUTRAL;
+	private Stack<Node> dfsStack=new Stack<Node>(); //Stack to maintain nodes
+	private boolean isPlaying= false; //game state
+	private MOVE myMove;
+	private boolean visited[]; //visited nodes?
+	public MOVE getMove(Game game, long timeDue) {
 
-    public MOVE getMove(Game game, long timeDue) {
-        //Place your game logic here to play the game as Ms Pac-Man
-    	//check
-    	myMove= MOVE.RIGHT;
-        return myMove;
-    }
+		//Start state
+		if(isPlaying!=true ||  game.wasPacManEaten()){
+			int current = game.getPacmanCurrentNodeIndex(); //Current Pacman index
+			Node[] dfsGraph= game.getCurrentMaze().graph; // current maze graph - set of nodes
+			Node currentPacManNode= dfsGraph[current]; //current node in the graph
+			dfsStack.push(currentPacManNode); // add
+			visited=new boolean[dfsGraph.length];//initialize to maze length
+			visited[currentPacManNode.nodeIndex]=true; //root 
+			isPlaying=true;   		
+		}
+
+		Node top=dfsStack.peek();
+
+		/*
+				1. Find neighbors of top
+		    	2. If neighbors not in visited, choose the one based on dfs 
+		    	3. Get its node index and the move
+		    	4. Mark the node as visited push it to stack 
+		    	5. update the move 
+		 */
+
+
+		Entry<MOVE, Integer> selectedNode = selectFromNeighbors(top);
+
+		//To visit
+		if(selectedNode != null){
+			visited[selectedNode.getValue()]=true;
+			Node entry=game.getCurrentMaze().graph[selectedNode.getValue()];
+			dfsStack.push(entry);
+			myMove = selectedNode.getKey();
+		}
+		//Null Handler
+		else{
+			myMove= game.getNextMoveTowardsTarget(top.nodeIndex, dfsStack.pop().nodeIndex, Constants.DM.PATH);
+			
+		}
+		return myMove;
+	}
+
+	private Entry<MOVE, Integer> selectFromNeighbors(Node top) {
+		for (Entry<MOVE, Integer> neighbour: top.neighbourhood.entrySet()) {
+			if (!visited[neighbour.getValue()]) {
+				return neighbour;
+			}
+		}
+		return null;
+	}
 }
+
